f9817924c2f8bf046bda5a4452b1c9b41896323e
Jerek0 <jeremy.minie@gmail.com>
2016-02-06 19:30:23 +0100

    Created structure + managed to switch between three scenes w/ touch command

---
+++ Assets/Scripts/util/SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+
+	public float delay = 0f;
+	public string sceneToLoad;
+
+	bool listening = false;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(startListening());
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(listening && Input.anyKey) {
+			Application.LoadLevel(sceneToLoad);
+		}
+
+		if (listening && Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended) {
+			Application.LoadLevel(sceneToLoad);
+		}
+	}
+
+	IEnumerator startListening() {       
+		yield return new WaitForSeconds(delay);
+		listening = true;
+	}
+}
