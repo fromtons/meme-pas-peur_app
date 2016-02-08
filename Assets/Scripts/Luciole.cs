bf5e7adb6cbec044847ac9596765a968755969c6
Jerek0 <jeremy.minie@gmail.com>
2016-02-08 19:08:04 +0100

    Lighten lucioles when clicking/tapping on them

--- Assets/Scripts/Luciole.cs
+++ Assets/Scripts/Luciole.cs
@@ -5,9 +5,17 @@ public class Luciole : MonoBehaviour {
 
 	public string pathToFollow = "";
 	public int pathDuration = 5;
+	public Sprite lightenedSprite;
+
+	private bool lightened = false;
+	private SpriteRenderer spriteRenderer;
+	private BoxCollider2D collider;
 
 	// Use this for initialization
 	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		collider = GetComponent<BoxCollider2D> ();
+
 		this.FollowPath();	
 	}
 
@@ -17,6 +25,23 @@ public class Luciole : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && !lightened) {
+			Vector2 touchPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch (0).position);
+			Collider2D hitCollider = Physics2D.OverlapPoint(touchPosition);
+			if (hitCollider == collider) {
+				spriteRenderer.sprite = lightenedSprite;
+				lightened = true;
+			}
+		}
 
-	}
+		if (Input.GetMouseButtonDown(0)) {
+			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D hitCollider = Physics2D.OverlapPoint(mousePosition);
+			if (hitCollider == collider) {
+				spriteRenderer.sprite = lightenedSprite;
+				lightened = true;
+			}
+		}
+	} 
 }
