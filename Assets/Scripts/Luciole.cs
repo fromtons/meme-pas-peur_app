cb8af46ab1a4179bda6c2e89644d171f0ec3ed97
Jerek0 <jeremy.minie@gmail.com>
2016-02-08 14:59:05 +0100

    Added lucioles w/ followpath scripts

---
+++ Assets/Scripts/Luciole.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Luciole : MonoBehaviour {
+
+	public string pathToFollow = "";
+	public int pathDuration = 5;
+
+	// Use this for initialization
+	void Start () {
+		this.FollowPath();	
+	}
+
+	void FollowPath() {
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathToFollow), "time", pathDuration, "easetype", iTween.EaseType.linear, "looptype", "loop"));	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
