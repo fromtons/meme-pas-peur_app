b940a972f1a8aefe97653a347655afc97466e9b7
Jerek0 <jeremy.minie@gmail.com>
2016-02-09 09:33:27 +0100

    Removed luciole on touch because onclick works for touch aswell

--- Assets/Scripts/Luciole.cs
+++ Assets/Scripts/Luciole.cs
@@ -25,18 +25,6 @@ public class Luciole : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && !lightened) {
-			Vector2 touchPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch (0).position);
-			Collider2D hitCollider = Physics2D.OverlapPoint(touchPosition);
-			if (hitCollider == collider) {
-				spriteRenderer.sprite = lightenedSprite;
-				lightened = true;
-
-				Chapter1Manager.checkLuciole();
-			}
-		}
-
 		if (Input.GetMouseButtonDown(0)) {
 			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Collider2D hitCollider = Physics2D.OverlapPoint(mousePosition);
