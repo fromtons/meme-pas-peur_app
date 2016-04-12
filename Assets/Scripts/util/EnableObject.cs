9d1683021cd932e6425f8c54437ce902e5889382
Jerek0 <jeremy.minie@gmail.com>
2016-04-12 10:28:57 +0200

    Added map scene w/ most of the skin + modal on chapter click

---
+++ Assets/Scripts/util/EnableObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableObject : MonoBehaviour {
+
+	public GameObject target;
+
+	public void toggleTarget() {
+		Debug.Log("TOGGLE");
+
+		if(!target.activeSelf)
+			target.SetActive (true);
+		else 
+			target.SetActive (false);
+	}
+}
