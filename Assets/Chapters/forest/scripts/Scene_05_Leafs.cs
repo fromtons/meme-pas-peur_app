adee981c0f6d3d5eb18b8e0b171f7e9049592c09
Jerek0 <jeremy.minie@gmail.com>
2016-04-13 18:28:55 +0200

    Forest scene 05 w/ leafs animations and timings properly done

---
+++ Assets/Chapters/forest/scripts/Scene_05_Leafs.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+
+public class Scene_05_Leafs : MonoBehaviour {
+	protected Animator animator;
+
+	protected const uint STATE_IDLE = 0;
+	protected const uint STATE_OPEN_SMALL = 1;
+	protected const uint STATE_OPEN_WIDE = 2;
+
+	protected uint _currentAnimationState = STATE_IDLE;
+	public uint CurrentAnimationState {
+		get {
+			return _currentAnimationState;
+		}
+
+		set {
+			animator.SetInteger ("state", (int)value);
+			_currentAnimationState = value;
+		}
+	}
+
+	public MicrophoneInput micInputToListen;
+	public float loudnessCap;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		StartCoroutine (TimeWaiter ());
+	}
+
+	IEnumerator TimeWaiter() {
+		yield return new WaitForSeconds(6f);
+		OpenSmall ();
+	}
+
+	void OpenSmall() {
+		CurrentAnimationState = STATE_OPEN_SMALL;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CurrentAnimationState == STATE_OPEN_SMALL) {
+			if (micInputToListen.loudness > loudnessCap) {
+				CurrentAnimationState = STATE_OPEN_WIDE;
+			}
+		}
+	}
+}
---
+++ Assets/Chapters/forest/scripts/Shaky.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shaky : MonoBehaviour {
+
+	public float intensity = 1f;
+	public float timeCycle = .1f;
+
+	public float delayRange = .5f;
+
+	int sign;
+	Vector3 initialRotation;
+
+	bool firstShake = true;
+
+	// Use this for initialization
+	void Start () {
+		sign = Random.value < .5 ? 1 : -1;
+		initialRotation = this.transform.localEulerAngles;
+		Forward ();
+	}
+
+	void Forward() {
+		Hashtable ht = new Hashtable ();
+		ht.Add ("rotation", new Vector3 (initialRotation.x, initialRotation.y, initialRotation.z + intensity * sign));
+		ht.Add ("time", timeCycle);
+		if (firstShake) {
+			ht.Add ("delay", delayRange * Random.value);
+			firstShake = false;
+		}
+		ht.Add ("islocal", true);
+		ht.Add ("oncomplete", "Backward");
+		ht.Add ("oncompletetarget", this.gameObject);
+		ht.Add ("easetype", "linear");
+		iTween.RotateTo (this.gameObject, ht);
+	}
+
+	void Backward() {
+		Hashtable ht = new Hashtable ();
+		ht.Add ("rotation", new Vector3 (initialRotation.x, initialRotation.y, initialRotation.z - intensity * sign));
+		ht.Add ("time", timeCycle);
+		ht.Add ("islocal", true);
+		ht.Add ("oncomplete", "Forward");
+		ht.Add ("oncompletetarget", this.gameObject);
+		ht.Add ("easetype", "linear");
+		iTween.RotateTo (this.gameObject, ht);
+	}
+}
