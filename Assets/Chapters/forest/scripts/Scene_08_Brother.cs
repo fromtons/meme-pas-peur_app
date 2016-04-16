5ea8051719046dc998844630dd5c2197b36de3c5
Jerek0 <jeremy.minie@gmail.com>
2016-04-16 12:33:43 +0200

    [Forest] Added scene 08 with first animations

---
+++ Assets/Chapters/forest/scripts/Scene_08_Brother.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene_08_Brother : MonoBehaviour {
+
+	protected Animator animator;
+
+	protected const uint STATE_IDLE = 0;
+	protected const uint STATE_WITH_BABY_OWL = 1;
+
+	protected uint _currentAnimationState = STATE_IDLE;
+	public uint CurrentAnimationState {
+		get {
+			return _currentAnimationState;
+		}
+
+		set {
+			animator.SetInteger ("state", (int)value);
+			_currentAnimationState = value;
+		}
+	}
+
+	public float delay = 3f;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator> ();
+
+		StartCoroutine (LaunchAnimation());
+	}
+
+	IEnumerator LaunchAnimation() {
+		yield return new WaitForSeconds (delay);
+		CurrentAnimationState = STATE_WITH_BABY_OWL;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
