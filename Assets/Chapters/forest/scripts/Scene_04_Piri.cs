eab6d990aa8e87255f5c3e482435a691c09127c8
Jerek0 <jeremy.minie@gmail.com>
2016-04-17 16:31:58 +0200

    [Forest][Scene04] Completely finished scene w/ good sound assets and timings

--- Assets/Chapters/forest/scripts/Scene_04_Piri.cs
+++ Assets/Chapters/forest/scripts/Scene_04_Piri.cs
@@ -13,15 +13,23 @@ public class Scene_04_Piri : MonoBehaviour {
 	public static int STATE_PROFILE_WALK = 1;
 	public static int STATE_SAD = 2;
 	public static int STATE_PROFILE_WALK_SAD = 3;
-	int _state = STATE_IDLE;
+	//int _state = STATE_IDLE;
 	public int state {
 		set {
-			_state = value;
+			//_state = value;
 			animator.SetInteger("state", value);
+
+			if (STATE_SAD == value || STATE_IDLE == value)
+				this.GetComponent<Talker> ().mouthManager.gameObject.SetActive (true);
+			else 
+				this.GetComponent<Talker> ().mouthManager.gameObject.SetActive (false);
 		}
 	}
 
 	Vector3 initialScale;
+
+	int insistCpt=0;
+	bool insist = true;
 
 	// Use this for initialization
 	void Start () {
@@ -45,10 +53,32 @@ public class Scene_04_Piri : MonoBehaviour {
 	void OnAnimationComplete() {
 		state = STATE_SAD;
 		this.gameObject.transform.localScale = new Vector3 (initialScale.x, initialScale.y, initialScale.z);;
+
+		TalkEventManager.TriggerTalkSet (new TalkEventArgs { ID = "piri", AudioClipId = 0, Autoplay = false });
+		TalkEventManager.TalkEnded += new TalkEventManager.TalkEvent (OnTalkEnded);
+	}
+
+	void OnTalkEnded(TalkEventArgs eventArgs) {
+		if (eventArgs.ID == "piri") {
+			if (eventArgs.AudioClipId != 1) {
+				StartCoroutine (Insist ());
+			} else {
+				OngletManager.instance.HighlightNextOnglet ();
+			} 
+		}
+	}
+
+	IEnumerator Insist() {
+		yield return new WaitForSeconds (8f);
+		if(insist)
+			TalkEventManager.TriggerTalkSet (new TalkEventArgs { ID = "piri", AudioClipId = ((insistCpt % 2)+2), Autoplay = true });
+		insistCpt++;
 	}
 
 	public void OnLightenedLucioles() {
+		insist = false;
 		state = STATE_IDLE;
+		TalkEventManager.TriggerTalkSet (new TalkEventArgs { ID = "piri", AudioClipId = 1, Autoplay = true });
 	}
 
 	// Update is called once per frame
