3d1585794e424530a33b8f3620aabbe461cb8e18
Jerek0 <jeremy.minie@gmail.com>
2016-04-17 20:28:55 +0200

    [Forest][10] Added dialogue + Back to map btn

---
+++ Assets/Chapters/forest/scripts/Scene_10_Piri.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene_10_Piri : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TalkEventManager.TriggerTalkSet (new TalkEventArgs { ID = "piri", AudioClipId = 0, Autoplay = false });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
