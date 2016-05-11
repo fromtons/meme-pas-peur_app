9f6f4c92bdc3a3538f2c03c97eb97bd366258354
Jerek0 <jeremy.minie@gmail.com>
2016-05-11 09:14:47 +0200

    [Forest][01] Delayed next onglet highlight + removed mushroom

---
+++ Assets/Chapters/forest/scripts/Scene_01_Manager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene_01_Manager : MonoBehaviour {
+
+	public float delay = 13f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DelayedHighlight());
+	}
+	
+	IEnumerator DelayedHighlight() {
+		yield return new WaitForSeconds(delay);
+		OngletManager.instance.HighlightNextOnglet();
+	}
+}
