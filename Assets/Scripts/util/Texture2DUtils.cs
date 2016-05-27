96313e7ebb2463a41a5bf1617851efcb810dde7a
Jerek0 <jeremy.minie@gmail.com>
2016-05-27 15:19:13 +0200

    [Global] Added Texture2DUtils w/ CropSquare function

---
+++ Assets/Scripts/util/Texture2DUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace MPP.Util
+{
+	public static class Texture2DUtils
+	{
+		public static Texture2D CropSquare(Texture2D texture) {
+			bool landscape = texture.width > texture.height;
+			int size = landscape ? texture.height : texture.width;
+
+			Texture2D newTexture = new Texture2D (size, size);
+
+			int minY = 0;
+			int minX = 0;
+			if (landscape) {
+				minX = (texture.width / 2) - (size / 2);
+			} else {
+				minY = (texture.height / 2) - (size / 2);
+			}
+
+			int newY = 0;
+			int newX = 0;
+			for (int y = minY; y < (minY+size); y++) {
+				newX = 0;
+				for (int x = minX; x < (minX+size); x++) {
+					newTexture.SetPixel(newX, newY, texture.GetPixel(x,y));
+					newX++;
+				}
+				newY++;
+			}
+			newTexture.Apply ();
+
+			return newTexture;
+		}
+	}
+}
+
