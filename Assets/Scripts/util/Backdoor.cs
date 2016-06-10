f8eb71f52b18b20a0d57c4ead98c55e13921f8be
Jerek0 <jeremy.minie@gmail.com>
2016-06-10 14:51:34 +0200

    Improved backdoors with cool down between touches

--- Assets/Scripts/util/Backdoor.cs
+++ Assets/Scripts/util/Backdoor.cs
@@ -9,6 +9,7 @@ namespace MPP.Utils {
 
 		public string ID;
 		BoxCollider2D _collider;
+		bool _disable = false;
 
 		// Use this for initialization
 		void Start () {
@@ -18,15 +19,21 @@ namespace MPP.Utils {
 		// Update is called once per frame
 		void Update () {
 
-			if (Input.GetMouseButton (0)) {
+			if (!_disable && Input.GetMouseButton (0)) {
 				Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 				Collider2D hitCollider = Physics2D.OverlapPoint(mousePosition);
 				if (hitCollider == _collider) {
 					Debug.Log ("backdoor toggled");
 					BackdoorEventManager.TriggerBackdoorToggle (new BackdoorEventArgs { ID = this.ID });
+					_disable = true;
+					StartCoroutine (CoolDown ());
 				}
 			}
-		
+		}
+
+		IEnumerator CoolDown() {
+			yield return new WaitForSeconds(.5f);
+			_disable = false;
 		}
 	}
 }
