59df4e84a34e75668b4f68f5c5abdaa3bcbce60f
Jerek0 <jeremy.minie@gmail.com>
2016-06-10 16:33:55 +0200

    Added advices for parents

---
+++ Assets/Scripts/menus/advices/TabButton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace MPP.Menus.Advices {
+	public class TabButton : MonoBehaviour {
+
+		public GameObject content;
+		public Color backgroundActive;
+		Color _backgroundInactive;
+		public Color textActive;
+		Color _textInactive;
+
+		public Text text;
+		Image _image;
+
+		bool _active = false;
+		public bool Active {
+			get { return _active; } 
+			set { 
+				_active = value; 
+				content.SetActive (_active);
+
+				if (_active) {
+					_image.color = backgroundActive;
+					text.color = textActive;
+				} else {
+					_image.color = _backgroundInactive;
+					text.color = _textInactive;
+				}
+			}
+		}
+
+		// Use this for initialization
+		void Awake () {
+			_image = this.GetComponent<Image> ();
+			_backgroundInactive = _image.color;
+			_textInactive = text.color;
+		}
+	}
+}
---
+++ Assets/Scripts/menus/advices/TabsManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MPP.Menus.Advices {
+	public class TabsManager : MonoBehaviour {
+
+		public List<TabButton> tabs;
+
+		// Use this for initialization
+		void Start () {
+			tabs [0].Active = true;
+		}
+		
+		public void SetActive(int id) {
+			foreach (TabButton tab in tabs) {
+				tab.Active = false;
+			}
+			tabs [id].Active = true;
+		}
+	}
+}
